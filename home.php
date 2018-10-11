c5ecca181391061e73b6eecd02d1a33cddd821e3
hthiyaga <35855544+hthiyaga@users.noreply.github.com>
2018-10-11 01:00:03 -0400

    Add files via upload

--- home.php
+++ home.php
@@ -10,6 +10,8 @@ include ("./func.php");
 <head>
 <title>Home</title>
 <link rel ="stylesheet" a href="css\home.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+        crossorigin="anonymous">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
     crossorigin="anonymous">
 
@@ -23,9 +25,9 @@ session_start();
 
 <br/>
 
-    <nav class="navbar navbar-expand-sm navbar-light bg-light mb-3">
+    <nav class="navbar navbar-expand-sm navbar-light mb-3" style="background-color:teal;">
         <div class="container">
-            <a class="navbar-brand" href="#">Welcome <?php 
+            <a class="navbar-brand" href="#" style="color:white;">Welcome <?php 
             require_once("./mysqli_connect.php");
     
             $mail_id=$_SESSION['email_id'];
@@ -41,17 +43,17 @@ session_start();
             
             
             ?></a>
-            <ul class="navbar-nav">
+            <ul class="navbar-nav" >
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="#" style="color:white;">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
+                    <a class="nav-link" href="#" style="color:white;">
                    
                     Account</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout.php">
+                <li class="nav-item" >
+                    <a class="nav-link" href="logout.php" style="color:white;">
                    
                     Logout</a>
                 </li>
@@ -59,39 +61,88 @@ session_start();
             </ul>
             </div>
     </nav>
+ <div id="content">
 
-    <div class="container ">
-    <form method="POST">
-    
-    <fieldset class="div1">
-    <label for="exampleTextarea"> It's time to post</label>
-    <br>
-    <textarea  id="content"name="content"  rows="4" cols="50"></textarea>
-    <br>
-    <input type="submit" style="margin-left:320px" class="btn btn-primary"  name="pos" value="Post" />
-    
-    </fieldset>
-    
-    
-    </form>
+   <div id="usertimeline">
 
-  <?php
-  // insertpost();
+    <div id="groupdetails">
+       <br>
+      <pre> <H5 style='margin-left:66px;'   >Groups    <a style='margin-left:8px;' href=# <i class="fas fa-plus"></i> </a></H5></pre>
+       
+       <br>
+   
+   <H6>
+       
 
-  if(isset($_POST['pos'])){
+       <?php
+          
+        require_once("./mysqli_connect.php");  
+        global $dbc;    
+        $mail_id=$_SESSION['email_id'];
+        $u_id = "SELECT user_id FROM `users` where email_id='$mail_id'";
+        $un_result= mysqli_query($dbc,$u_id);
+        $rowu= mysqli_fetch_array($un_result);
+        $user_id= $rowu['user_id'];
+        $g_name = "SELECT groups.group_name,groups.group_id FROM `groups` INNER JOIN `user_groups` where user_groups.user_id='$user_id' AND groups.group_id=user_groups.group_id";
+        $gn_result= mysqli_query($dbc,$g_name);
+       while( $rowg= mysqli_fetch_array($gn_result)){
+
+           $group_name=$rowg['group_name'];
+           $group_id = $rowg['group_id'];
+        echo " <a href='home.php?id=$group_id'> $group_name </a><br> <br>";
+
+       }
         
-    require_once("./mysqli_connect.php");
+
+       ?>
+       
+  </H6>
+    </div>
+     
+   </div>
+ 
+      
+ </div>
+
+
+ <div class="clearfix" id='darea'>
+
+    <?php
+   require_once("./mysqli_connect.php");  
+   global $dbc;
+   if(isset($_GET['id'])){
+      
+       echo "<form method='POST'>
     
+              <fieldset>
+              <label for='exampleTextarea' style='margin-left: 10px;'> It's time to post</label>
+              <br><br>
+              
+              <textarea style='width:80%;'  id='content'name='content' placeholder='Type something.... :)'  rows='2' cols='100'></textarea>
+
+              <input type='submit' style='margin-top:-30px;'class='btn btn-primary'  name='pos' value='Post' />
+              </fieldset>
+              </form>";
+    
+  //Insert post
+      
+
+     if(isset($_POST['pos'])){
+        
+   
+    $group_id =  $_GET['id'];
     $content =$_POST['content'];
     $mail_id=$_SESSION['email_id'];
     $user_query = "SELECT user_id FROM `users` WHERE email_id='$mail_id'";
+  
+
   
     $user_result= mysqli_query($dbc,$user_query);
     $row= mysqli_fetch_array($user_result);
     $user_id= $row['user_id'];
     
 
-    $insert = "insert into posts(user_id, post_content, post_timestamp) values('$user_id','$content', NOW())";
+    $insert = "insert into posts(user_id, post_content, post_timestamp,  group_id) values('$user_id','$content', NOW(),'$group_id')";
  
     $run = mysqli_query($dbc,$insert);
 
@@ -105,14 +156,93 @@ session_start();
     }
 
    }
+
+
+   // Display the posts
+
+       $group_id =  $_GET['id'];
+       
+       $g_posts = "SELECT * FROM `posts` WHERE group_id='$group_id'";
+
+       $r_posts =  mysqli_query($dbc,$g_posts);
+       while($row_posts=mysqli_fetch_array($r_posts)){
+    
+         $post_id = $row_posts['post_id'];
+         $user_id = $row_posts['user_id'];
+         $content = $row_posts['post_content'];
+         $post_tstamp = $row_posts['post_timestamp'];
+       
+    
+      //fetch user details regarding the posts
+    
+       $user="SELECT * FROM `users` WHERE user_id='$user_id' AND posts='yes' ";
+       $r_user= mysqli_query($dbc,$user);
+       $row_user=mysqli_fetch_array($r_user);
+       $user_name = $row_user['user_name'];
+    
+       //display posts
+      
+       echo "
+       <br>
+       <div id='dis'>
+       <div id= 'posts' style='float:left;' >
+        
+       <H6><a href='user_profile.php? user_id=$user_id'>$user_name</a></H6>
+       </div>
+       <p id='tstamp' style='margin-left:100px;'>$post_tstamp</p>
+       <p>$content</p>
+       
+       <i class='far fa-thumbs-up'></i>  
+       <i style='margin-left:8px;'class='far fa-thumbs-down'></i>  
+       <i style='margin-left:8px;'class='fas fa-reply'></i>
+       <a href='reply.php?post_id=$post_id' style='font-size:10px;' >
+          reply</a>
+       
+       </div>
+       
+          ";
+       }
+
+
+   }
+  else{
+
+    echo" <div id='index'>
+      <H1 style='text-align:center;'>  Select the group </H1>
+      <img src ='./display.png'/></div>";
+  }
+
+       
+
+
+
  ?>
+
+
+ </div>
+    <div class="container ">
+    <!-- <form method="POST">
+    
+    <fieldset class="div1">
+    <label for="exampleTextarea"> It's time to post</label>
+    <br>
+    <textarea  id="content"name="content"  rows="4" cols="50"></textarea>
+    <br>
+    <input type="submit" style="margin-left:320px" class="btn btn-primary"  name="pos" value="Post" />
+    
+    </fieldset>
+    
+    
+    </form> -->
+
+ 
   
-    <div class="posts" style="padding-left:25%;">
+    <!--  <div class="posts" style="padding-left:16%;">
 
         <h5>RECENT</h5>
         <?php
 
-        // getpost();
+        getpost();
 
         require_once('./mysqli_connect.php');
         global $dbc;
@@ -149,8 +279,7 @@ session_start();
        
           ";
        }
-        ?>
-
+        ?>  -->
 
     </div>
     </div>

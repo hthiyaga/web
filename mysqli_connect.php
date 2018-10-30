ad76d062696baac5b314a6dbf0bf6397bd8c9885
hthiyaga <35855544+hthiyaga@users.noreply.github.com>
2018-10-30 01:02:01 -0400

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -6,6 +6,5 @@ DEFINE('DB_HOST','localhost');
 DEFINE('DB_NAME', 'web_prj');
 
 
-$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD,DB_NAME)
-OR dies('Could not connect'. mysqli_connect_error());
+$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD,DB_NAME);
 ?>

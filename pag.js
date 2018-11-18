fa3a991d1827d276f90835851974bb72bffaad23
hthiyaga <hthiy001@odu.edu>
2018-11-18 17:14:17 -0500

    udpated

--- pag.js
+++ pag.js
@@ -187,175 +187,197 @@ $(document).ready(function(){
 
 //upload image to posts
 
-  $(".form").on('submit', function(e){ 
+$(document).on('click', '.upload_image', function(e){ 
 
   var group_id = e.currentTarget.id;
+  var property= document.getElementById("file").files[0];
+  var image_name =property.name;
+  var image_extension = image_name.split(".").pop().toLowerCase();
+  if(jQuery.inArray(image_extension,['gif','png','jpg','jpeg']) == -1)
+  {
+    alert("Invalid image File");
+  }
+  var image_size=property.size;
+  if(image_size > 2000000){
+
+    alert("Image file size is very big");
+  }
+
+  else{
+
+    var form_data = new FormData();
+    form_data.append("file",property);
+    form_data.append("id",group_id);
+  }
   e.preventDefault();
   
   $.ajax({
-    url:'comment.php',
+    url:'upload.php',
     type: 'post',
-    data: new FormData(this),
-    dataType: 'text',
+    data: form_data,
+    contentType:false,
+    cache:false,
+    processData:false,
+
     success: function(data){
-      var obj = JSON.parse(data);
-      str="";
-      console.log(data);
-     obj['messages'].forEach(function(e){
-           if(e['user_image']==""){
+     //  var obj = JSON.parse(data);
+     //  str="";
+     //  console.log(data);
+     // obj['messages'].forEach(function(e){
+     //       if(e['user_image']==""){
                 
-                $dp ="default.jpg";
-                 }
-                  else{
-                       $dp =e['user_image'];
-                       }
+     //            $dp ="default.jpg";
+     //             }
+     //              else{
+     //                   $dp =e['user_image'];
+     //                   }
 
      
      
-      str+= "<br>";
+     //  str+= "<br>";
       
     
-      str+= "<div id='dis"+e['post_id']+"' class='dis'>";
-      str+= "<div>";
-      str+= "<img width='40' height='40' src ='img/"+$dp+"' alt= 'ddp'>";
-      str+= "</div><br>";
-      str+= "<div id= 'posts' style='float:left;' >";
-      str+= "<H6><a href='userprofile.php?'>"+ e['user_name']+"</a></H6>";
-      str+= "</div>";
-      str+= "<p id='tstamp' style='margin-left:100px;'>"+e['post_timestamp']+"</p>";
-      str+= "<pre><code>"+e['code_content']+"</code></pre>";
+     //  str+= "<div id='dis"+e['post_id']+"' class='dis'>";
+     //  str+= "<div>";
+     //  str+= "<img width='40' height='40' src ='img/"+$dp+"' alt= 'ddp'>";
+     //  str+= "</div><br>";
+     //  str+= "<div id= 'posts' style='float:left;' >";
+     //  str+= "<H6><a href='userprofile.php?'>"+ e['user_name']+"</a></H6>";
+     //  str+= "</div>";
+     //  str+= "<p id='tstamp' style='margin-left:100px;'>"+e['post_timestamp']+"</p>";
+     //  str+= "<pre><code>"+e['code_content']+"</code></pre>";
 
-      var post_id= e['post_id'];
+     //  var post_id= e['post_id'];
 
       //userlike
-       $.ajax({ 
+      //  $.ajax({ 
 
-        async:false,
-        url:'comment.php',
-        type: 'post',
-        data:{ 'userLiked':post_id},
-        dataType: 'text',
-        success: function(data){ 
+      //   async:false,
+      //   url:'comment.php',
+      //   type: 'post',
+      //   data:{ 'userLiked':post_id},
+      //   dataType: 'text',
+      //   success: function(data){ 
          
-          var userLiked =  parseInt(data);
+      //     var userLiked =  parseInt(data);
 
-            if(e['archive_action'] != "archive")
-            {
-              if(userLiked == 1){ 
-              str+=  "<i class='fa fa-thumbs-up like-btn' data-id="+ post_id + "></i>  ";
-                  }
-               else {
-              str+= "<i class='fa fa-thumbs-o-up like-btn' data-id="+ post_id + "></i>";
+      //       if(e['archive_action'] != "archive")
+      //       {
+      //         if(userLiked == 1){ 
+      //         str+=  "<i class='fa fa-thumbs-up like-btn' data-id="+ post_id + "></i>  ";
+      //             }
+      //          else {
+      //         str+= "<i class='fa fa-thumbs-o-up like-btn' data-id="+ post_id + "></i>";
         
-                }
+      //           }
               
-            }
+      //       }
 
-            else {
+      //       else {
 
-                if(userLiked == 1){ 
-                  str+=  "<i class='fa fa-thumbs-up'></i>  ";
-                  }
-               else {
-                 str+= "<i class='fa fa-thumbs-o-up'></i>";
+      //           if(userLiked == 1){ 
+      //             str+=  "<i class='fa fa-thumbs-up'></i>  ";
+      //             }
+      //          else {
+      //            str+= "<i class='fa fa-thumbs-o-up'></i>";
         
-                }
-            }
+      //           }
+      //       }
         
 
-           }
+      //      }
 
-            });
-            str +="<span class='likes'>";
-             $.ajax({ 
+      //       });
+      //       str +="<span class='likes'>";
+      //        $.ajax({ 
 
-        async:false,
-        url:'comment.php',
-        type: 'post',
-        data:{ 'getLikes':post_id},
-        dataType: 'text',
-        success: function(data){ 
-            console.log(data);
+      //   async:false,
+      //   url:'comment.php',
+      //   type: 'post',
+      //   data:{ 'getLikes':post_id},
+      //   dataType: 'text',
+      //   success: function(data){ 
+      //       console.log(data);
                    
-                   str += data;
-           }
+      //              str += data;
+      //      }
 
-            });
-            str +="</span>"
-            str +="&nbsp;&nbsp;&nbsp;&nbsp;";
-        //userdislike
-        $.ajax({ 
+      //       });
+      //       str +="</span>"
+      //       str +="&nbsp;&nbsp;&nbsp;&nbsp;";
+      //   //userdislike
+      //   $.ajax({ 
 
-        async:false,
-        url:'comment.php',
-        type: 'post',
-        data:{ 'userDisliked':post_id},
-        dataType: 'text',
-        success: function(data){ 
+      //   async:false,
+      //   url:'comment.php',
+      //   type: 'post',
+      //   data:{ 'userDisliked':post_id},
+      //   dataType: 'text',
+      //   success: function(data){ 
            
-          var userDisliked =  parseInt(data);
+      //     var userDisliked =  parseInt(data);
 
-          if(e['archive_action'] != "archive"){
-                if(userDisliked == 1){
+      //     if(e['archive_action'] != "archive"){
+      //           if(userDisliked == 1){
 
-                str+=  "<i class='fa fa-thumbs-down dislike-btn' data-id="+ post_id + "></i>  ";
-              }
+      //           str+=  "<i class='fa fa-thumbs-down dislike-btn' data-id="+ post_id + "></i>  ";
+      //         }
 
-                    else{
+      //               else{
            
-                str+= "<i class='fa fa-thumbs-o-down dislike-btn' data-id="+ post_id + "></i>";
+      //           str+= "<i class='fa fa-thumbs-o-down dislike-btn' data-id="+ post_id + "></i>";
             
-                 }
+      //            }
 
-          }
+      //     }
 
-          else{
-                  if(userDisliked == 1){
+      //     else{
+      //             if(userDisliked == 1){
 
-                str+=  "<i class='fa fa-thumbs-down'></i>  ";
-              }
+      //           str+=  "<i class='fa fa-thumbs-down'></i>  ";
+      //         }
 
-                    else{
+      //               else{
            
-                str+= "<i class='fa fa-thumbs-o-down'></i>";
+      //           str+= "<i class='fa fa-thumbs-o-down'></i>";
             
-                 }
+      //            }
 
-          }
+      //     }
          
           
 
-           }
+      //      }
 
-            });
+      //       });
 
 
-            str +="<span class='dislikes'>";
-             $.ajax({ 
+      //       str +="<span class='dislikes'>";
+      //        $.ajax({ 
 
-        async:false,
-        url:'comment.php',
-        type: 'post',
-        data:{ 'getDislikes':post_id},
-        dataType: 'text',
-        success: function(data){ 
-            console.log(data);
+      //   async:false,
+      //   url:'comment.php',
+      //   type: 'post',
+      //   data:{ 'getDislikes':post_id},
+      //   dataType: 'text',
+      //   success: function(data){ 
+      //       console.log(data);
                    
-                   str += data;
-           }
+      //              str += data;
+      //      }
 
-            });
-            str +="</span>"
-            str +="&nbsp;&nbsp;&nbsp;&nbsp;";
+      //       });
+      //       str +="</span>"
+      //       str +="&nbsp;&nbsp;&nbsp;&nbsp;";
 
-      //before closing
-      str+="</div>";
-      str+= "<br>";
+      // //before closing
+      // str+="</div>";
+      // str+= "<br>";
       
 
-    });
+    // });
       
-      $('#posts').prepend(str);
+      $('#posts').prepend(data);
       $('textarea[name=content]').val('');
 
 
@@ -371,4 +393,6 @@ $(document).ready(function(){
     
 });
 
+
+  
 	});

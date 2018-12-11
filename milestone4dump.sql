090ffd70be411f57f1a234475d7e347b60426d34
hthiyaga <hthiy001@odu.edu>
2018-12-11 14:27:50 -0500

    dump

--- milestone4dump.sql
+++ milestone4dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2018 at 07:06 AM
+-- Generation Time: Dec 11, 2018 at 08:22 PM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
@@ -20,7 +20,6 @@ SET time_zone = "+00:00";
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `web_prj` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `web_prj`;
-
 --
 -- Database: `web_prj`
 --
@@ -60,17 +59,6 @@ CREATE TABLE `chats` (
   `chat_timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `chat_user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `chats`
---
-
-INSERT INTO `chats` (`chat_id`, `user_id`, `chat_content`, `chat_timestamp`, `chat_user_id`) VALUES
-(22, 6, 'asd', '2018-12-06 16:21:12', 16),
-(23, 1, 'Hi Hari', '2018-12-06 16:48:09', 61),
-(24, 6, 'Hey Mater', '2018-12-06 16:50:30', 16),
-(32, 6, 'kk', '2018-12-07 01:14:23', 26),
-(33, 6, 'Hi panda', '2018-12-07 04:13:24', 16);
 
 -- --------------------------------------------------------
 
@@ -163,17 +151,7 @@ INSERT INTO `posts` (`post_id`, `user_id`, `post_content`, `code_content`, `imag
 (451, 6, 'Third milestone is due....:(', '', '', '', '', '2018-11-20 05:24:42', 1),
 (452, 1, 'Thanksgiving holidays.......! yaaay', '', '', '', '', '2018-11-20 05:25:30', 1),
 (453, 6, 'a', '', '', '', '', '2018-11-20 21:22:42', 4),
-(454, 6, '&lt;!DOCTYPE HTML&gt;\n&lt;html&gt;\n    &lt;head&gt;\n        &lt;title&gt;Register&lt;/title&gt;\n    &lt;/head&gt;\n    &lt;body&gt;\n        &lt;form action=&quot;register.php&quot; method=&quot;POST&quot;&gt;\n            Username: &lt;input type=&quot;text&quot; name=&quot;username&quot;&gt;\n            &lt;br/&gt;\n            Password: &lt;input type=&quot;password&quot; name=&quot;password&quot;&gt;\n            &lt;br/&gt;\n            Confirm Password: &lt;input type=&quot;password&quot; name=&quot;confirmPassword&quot;&gt;\n            &lt;br/&gt;\n            Email: &lt;input type=&quot;text&quot; name=&quot;email&quot;&gt;\n            &lt;br/&gt;\n            &lt;input type=&quot;submit&quot; name=&quot;submit&quot; value=&quot;Register&quot;&gt; or &lt;a href=&quot;login.php&quot;&gt;Log in&lt;/a&gt;\n        &lt;/form&gt;\n    &lt;/body&gt;\n&lt;/html&gt;\n&lt;?php\n    require(\'connect.php\');\n    $username = $_POST[\'username\'];\n    $password = $_POST[\'password\'];\n    $confirmPassword = $_POST[\'confirmPassword\'];\n    $email = $_POST[\'email\'];\n\n    if(isset($_POST[&quot;submit&quot;])){\n        if($query = mysql_query(&quot;INSERT INTO users (\'id\', \'username\', \'password\', \'email\') VALUES(\'\', \'&quot;.$username.&quot;\', \'&quot;.$password.&quot;\', \'&quot;.$email.&quot;\')&quot;)){\n            echo &quot;Success&quot;;\n        }else{\n            echo &quot;Failure&quot; . mysql_error();\n        }\n    }\n?&gt;', '', '', '', '', '2018-11-20 21:38:54', 1),
-(455, 6, '', '&lt;!DOCTYPE HTML&gt;\n&lt;html&gt;\n    &lt;head&gt;\n        &lt;title&gt;Register&lt;/title&gt;\n    &lt;/head&gt;\n    &lt;body&gt;\n        &lt;form action=&quot;register.php&quot; method=&quot;POST&quot;&gt;\n            Username: &lt;input type=&quot;text&quot; name=&quot;username&quot;&gt;\n            &lt;br/&gt;\n            Password: &lt;input type=&quot;password&quot; name=&quot;password&quot;&gt;\n            &lt;br/&gt;\n            Confirm Password: &lt;input type=&quot;password&quot; name=&quot;confirmPassword&quot;&gt;\n            &lt;br/&gt;\n            Email: &lt;input type=&quot;text&quot; name=&quot;email&quot;&gt;\n            &lt;br/&gt;\n            &lt;input type=&quot;submit&quot; name=&quot;submit&quot; value=&quot;Register&quot;&gt; or &lt;a href=&quot;login.php&quot;&gt;Log in&lt;/a&gt;\n        &lt;/form&gt;\n    &lt;/body&gt;\n&lt;/html&gt;\n&lt;?php\n    require(\'connect.php\');\n    $username = $_POST[\'username\'];\n    $password = $_POST[\'password\'];\n    $confirmPassword = $_POST[\'confirmPassword\'];\n    $email = $_POST[\'email\'];\n\n    if(isset($_POST[&quot;submit&quot;])){\n        if($query = mysql_query(&quot;INSERT INTO users (\'id\', \'username\', \'password\', \'email\') VALUES(\'\', \'&quot;.$username.&quot;\', \'&quot;.$password.&quot;\', \'&quot;.$email.&quot;\')&quot;)){\n            echo &quot;Success&quot;;\n        }else{\n            echo &quot;Failure&quot; . mysql_error();\n        }\n    }\n?&gt;', '', '', '', '2018-11-20 21:39:01', 1),
-(456, 6, 'asdasd', '', '', '', '', '2018-12-04 03:20:50', 1),
-(457, 6, 'asdasd', '', '', '', '', '2018-12-04 03:21:23', 1),
-(458, 6, 'asd', '', '', '', '', '2018-12-04 03:22:52', 1),
-(459, 6, 'asdasdasd', '', '', '', '', '2018-12-04 03:30:39', 3),
-(460, 6, 'asdasd', '', '', '', '', '2018-12-04 03:32:26', 1),
-(469, 6, 'asdsss', '', '', '', '', '2018-12-05 04:24:12', 1),
-(515, 6, '', 'a\n  g', '', '', '', '2018-12-11 00:18:50', 1),
-(518, 6, '', '', '', 'CHECK IN.pdf', '', '2018-12-11 00:20:38', 1),
-(519, 6, '', '', '', 'axis bank reference number.txt', '', '2018-12-11 00:37:37', 1);
+(459, 6, 'asdasdasd', '', '', '', '', '2018-12-04 03:30:39', 3);
 
 -- --------------------------------------------------------
 
@@ -246,7 +224,7 @@ INSERT INTO `users` (`user_id`, `user_name`, `email_id`, `password`, `user_image
 (3, 'Doc Hudson', 'hornet@rsprings.gov', '@doc', 'https://www.gravatar.com/avatar/936cb3b6e701c67af2337eac72f8451d', '1', 'yes', 'yes', '', 0),
 (4, 'Finn McMissile', 'topsecret@agent.org', '@mcmissile', '', '0', NULL, '', '', 0),
 (5, 'Lighting McQueen', 'kachow@rusteze.com', '@mcqueen', '', '0', NULL, '', '', 0),
-(6, 'Hari T', 'hari', 'hari', '', '0', 'yes', 'yes', 'yes', 0),
+(6, 'Hari T', 'hari', 'hari', '6.jpg', '0', 'yes', 'yes', 'yes', 0),
 (7, 'Saketh K', 'saketh', 'saketh', '', '0', 'yes', '', '', 0),
 (20, 'admin', 'admin', 'admin', '', '0', 'yes', '', 'yes', 0),
 (22, 'hsquare', 'hariharan0907@gmail.com', 'hari', '', '0', 'NULL', '', '', 0);
@@ -369,7 +347,7 @@ ALTER TABLE `archive_info`
 -- AUTO_INCREMENT for table `chats`
 --
 ALTER TABLE `chats`
-  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `chat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `comments`
@@ -393,19 +371,19 @@ ALTER TABLE `otp_expiry`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=520;
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=523;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `user_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `user_groups`
 --
 ALTER TABLE `user_groups`
-  MODIFY `ugroup_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=148;
+  MODIFY `ugroup_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=149;
 
 --
 -- Constraints for dumped tables
